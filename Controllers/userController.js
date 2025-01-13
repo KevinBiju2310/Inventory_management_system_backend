647ead77e0a376ff69c63bad8b8db36198d24c57
Kevin <kevinjamesbiju2000@gmail.com>
2025-01-13 23:34:43 +0530

    changes to userController

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -43,11 +43,13 @@ const signIn = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite:"None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite:"None",
       maxAge: 15 * 60 * 1000,
     });
     res.status(200).json({ message: "Login successfull", user });
