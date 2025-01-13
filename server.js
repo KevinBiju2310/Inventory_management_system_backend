c1db7a48a590103e03cc73648fbe2f8773b7f17b
Kevin <kevinjamesbiju2000@gmail.com>
2025-01-13 23:22:56 +0530

    changes to urls

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://inventory-management-system-frontend-vb56.onrender.com",
     credentials: true,
   })
 );
