c0c7d77be526291daadb5ba692a84798c8a96a65
Kevin <kevinjamesbiju2000@gmail.com>
2025-02-19 19:00:42 +0530

    removed slash from url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://inventory-management-system-frontend-theta.vercel.app/",
+    origin: "https://inventory-management-system-frontend-theta.vercel.app",
     // origin: "http://localhost:5173",
     credentials: true,
   })
