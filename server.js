9a8d065f225b228f9167181d21463db61883604e
Kevin <kevinjamesbiju2000@gmail.com>
2025-02-19 18:49:17 +0530

    frontend vercel

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://inventory-management-system-frontend-vb56.onrender.com",
+    origin: "https://inventory-management-system-frontend-theta.vercel.app/",
     // origin: "http://localhost:5173",
     credentials: true,
   })
